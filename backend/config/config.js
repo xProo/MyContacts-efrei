a90944c9f698efcef11376fb7859f178a577ea05
sohane-efrei <votre-email-efrei@efrei.net>
2025-09-16 15:13:26 +0200

    amelioration du code

--- backend/config/config.js
+++ backend/config/config.js
@@ -5,7 +5,9 @@ module.exports = {
     "mongodb+srv://sohanechamen_db_user:4zeIaWqQyFUAB3LA@mycontactcluster.mmmlkqq.mongodb.net/?retryWrites=true&w=majority&appName=myContactCluster",
 
   // Configuration JWT
-  JWT_SECRET: process.env.JWT_SECRET || "votre_secret_jwt_super_securise_2024",
+  JWT_SECRET:
+    process.env.JWT_SECRET ||
+    "MyContacts_App_2024_SecureJWT_Key_9x7k2m8f4e1a6b3c5d7h",
   JWT_EXPIRE: process.env.JWT_EXPIRE || "7d",
 
   // Configuration du serveur
